356190952294351172f49e4264383aa3d0f1ab22
olegk <oligkondr@mail.ru>
2025-08-26 15:01:10 +0300

    Add tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,19 @@
-<p>Home page</p>
-@auth
-    <p>Hello {{Auth::user()->name}}!</p>
-    <p><a href="{{route('logout')}}">logout</a></p>
-@else
-    <p><a href="{{route('login')}}">login</a></p>
-    <p><a href="{{route('register')}}">register</a></p>
-@endauth
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/> @vite('resources/css/app.css')
+</head>
+<body>
+<h1 class="text-3xl font-bold "> Hello world! </h1>
+{{--<p>Home page</p>--}}
+{{--@auth--}}
+{{--    <p>Hello {{Auth::user()->name}}!</p>--}}
+{{--    <p><a href="{{route('logout')}}">logout</a></p>--}}
+{{--@else--}}
+{{--    <p><a href="{{route('login')}}">login</a></p>--}}
+{{--    <p><a href="{{route('register')}}">register</a></p>--}}
+{{--@endauth--}}
+
+</body>
+</html>
